a8bfea74327190999e71452525ec11fb8e569b76
Josh Brown <josh.brown@e-gineering.com>
2018-08-01 13:14:26 -0400

    Initial Commit

---
+++ RZBCocoaPods/RZBCocoaPods.h
@@ -0,0 +1,19 @@
+//
+//  RZBCocoaPods.h
+//  RZBCocoaPods
+//
+//  Created by Josh Brown on 8/1/18.
+//  Copyright © 2018 E-gineering. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RZBCocoaPods.
+FOUNDATION_EXPORT double RZBCocoaPodsVersionNumber;
+
+//! Project version string for RZBCocoaPods.
+FOUNDATION_EXPORT const unsigned char RZBCocoaPodsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RZBCocoaPods/PublicHeader.h>
+
+
